b14315715c11672c027e5f77b99755586c00d3f2
elizabeth archibong <earchibong@gmail.com>
2023-01-11 15:00:33 +0100

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -29,3 +29,17 @@
 #    type = "S"
 #  }
 #}
+
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  backend "remote" {
+    organization = "mintedtech"
+
+    workspaces {
+      name = "terraform-cloud"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
