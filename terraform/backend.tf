e763324babbd1c290afbd80834b144b77703c5c2
elizabeth archibong <earchibong@gmail.com>
2023-01-13 15:20:55 +0100

    test files update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -29,7 +29,7 @@
 #    type = "S"
 #  }
 #}
-#
+
 
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
